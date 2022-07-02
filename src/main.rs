2cf089b78dfa1c9dbd55ab735ffec74aa57341e0
flooijdt <flooijdt@posteo.de>
2022-07-02 14:08:48 -0300

    printing json

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use serde_json::Value;
 
 fn main() {
-    let response = reqwest::blocking::get("https://storage.googleapis.com/juntossomosmais-code-challenge/input-backend.json").expect("unable to read the origin json.");
+    let response = reqwest::blocking::get("https://storage.googleapis.com/juntossomosmais-code-challenge/input-backend.json").expect("unable to get the origin json.");
     let json_response: Value = serde_json::from_str(response.text().expect("unable to converte Response to String.").as_str()).expect("unable to convert &str to serde_json::Value.");
 
     println!("{}",json_response);
