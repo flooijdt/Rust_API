a8f4e49c37982a5209945875c7cb587acd96da8a
flooijdt <flooijdt@posteo.de>
2022-08-01 17:22:31 -0300

    separating structs from server

--- src/structs.rs
+++ src/structs.rs
@@ -1,66 +1,70 @@
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+use serde_json::Value;
+
     #[derive(Debug, Deserialize, Clone, Serialize)]
     pub struct Dob {
-        age: u32,
-        date: String,
+        pub age: u32,
+        pub date: String,
     }
 
     #[derive(Debug, Deserialize, Clone, Serialize)]
     pub struct Location {
-        city: String,
-        coordinates: Coordinates,
-        postcode: u32,
-        state: String,
-        street: String,
-        timezone: Timezone,
+        pub city: String,
+        pub coordinates: Coordinates,
+        pub postcode: u32,
+        pub state: String,
+        pub street: String,
+        pub timezone: Timezone,
     }
 
     #[derive(Debug, Deserialize, Clone, Serialize)]
     pub struct Coordinates {
-        latitude: String,
-        longitude: String,
+        pub latitude: String,
+        pub longitude: String,
     }
 
     #[derive(Debug, Deserialize, Clone, Serialize)]
     pub struct Timezone {
-        description: String,
-        offset: String,
+        pub description: String,
+        pub offset: String,
     }
 
     #[derive(Debug, Deserialize, Clone, Serialize)]
     pub struct Name {
-        first: String,
-        last: String,
-        title: String,
+        pub first: String,
+        pub last: String,
+        pub title: String,
     }
 
     #[derive(Debug, Deserialize, Clone, Serialize)]
     pub struct Picture {
-        large: String,
-        medium: String,
-        thumbnail: String,
+        pub large: String,
+        pub medium: String,
+        pub thumbnail: String,
     }
 
     #[derive(Debug, Deserialize, Clone, Serialize)]
     pub struct Registered {
-        age: u32,
-        date: String,
+        pub age: u32,
+        pub date: String,
     }
 
     #[derive(Debug, Deserialize, Clone, Serialize)]
     pub struct ClientUnited {
-        cell: String,
-        dob: Dob,
-        email: String,
-        gender: String,
-        location: Location,
-        name: Name,
-        phone: String,
-        picture: Picture,
-        registered: Registered,
+        pub cell: String,
+        pub dob: Dob,
+        pub email: String,
+        pub gender: String,
+        pub location: Location,
+        pub name: Name,
+        pub phone: String,
+        pub picture: Picture,
+        pub registered: Registered,
     }
 
     impl ClientUnited {
-        fn new(value: Value) -> Self {
+        pub fn new(value: Value) -> Self {
             let client: ClientUnited = serde_json::from_value(value).unwrap();
             client
         }
@@ -69,62 +73,89 @@
    // convert response to Reader, for file tempering.
     #[derive(Debug, Deserialize, Clone, Serialize)]
     pub struct ClientCSV {
-        gender: String,
-        name__title: String,
-        name__first: String,
-        name__last: String,
-        location__street: String,
-        location__city: String,
-        location__state: String,
-        location__postcode: u32,
-        location__coordinates__latitude: f64,
-        location__coordinates__longitude: f64,
-        location__timezone__offset: String,
-        location__timezone__description: String,
-        email: String,
-        dob__date: String,
-        dob__age: u32,
-        registered__date: String,
-        registered__age: u32,
-        phone: String,
-        cell: String,
-        picture__large: String,
-        picture__medium: String,
-        picture__thumbnail: String,
+        pub gender: String,
+        pub name__title: String,
+        pub name__first: String,
+        pub name__last: String,
+        pub location__street: String,
+        pub location__city: String,
+        pub location__state: String,
+        pub location__postcode: u32,
+        pub location__coordinates__latitude: f64,
+        pub location__coordinates__longitude: f64,
+        pub location__timezone__offset: String,
+        pub location__timezone__description: String,
+        pub email: String,
+        pub dob__date: String,
+        pub dob__age: u32,
+        pub registered__date: String,
+        pub registered__age: u32,
+        pub phone: String,
+        pub cell: String,
+        pub picture__large: String,
+        pub picture__medium: String,
+        pub picture__thumbnail: String,
     }
      // create final Client struct according to desired output.
     #[derive(Debug, Deserialize, Clone, Serialize)]
     pub struct Client {
-        r#type: String,
-        gender: String,
-        name: Name,
-        location: Location2,
-        email: String,
-        birthday: String,
-        registered: String,
-        telephoneNumbers: Vec<String>,
-        mobileNumbers: Vec<String>,
-        picture: Picture,
-        nationality: String,
+        pub id: ClientId,
+        pub r#type: String,
+        pub gender: String,
+        pub name: Name,
+        pub location: Location2,
+        pub email: String,
+        pub birthday: String,
+        pub registered: String,
+        pub telephoneNumbers: Vec<String>,
+        pub mobileNumbers: Vec<String>,
+        pub picture: Picture,
+        pub nationality: String,
     }
 
     #[derive(Debug, Deserialize, Clone, Serialize)]
     pub struct Location2 {
-        region: String,
-        city: String,
-        coordinates: Coordinates,
-        postcode: u32,
-        state: String,
-        street: String,
-        timezone: Timezone,
+        pub region: String,
+        pub city: String,
+        pub coordinates: Coordinates,
+        pub postcode: u32,
+        pub state: String,
+        pub street: String,
+        pub timezone: Timezone,
     }
     // clasification in regard to coordinates: special, labourious or normal.
 
-    pub struct LocationCorrdinates {
-        minlon: f64,
-        minlat: f64,
-        maxlon: f64,
-        maxlat: f64,
+    pub struct LocationCoordinates {
+        pub minlon: f64,
+        pub minlat: f64,
+        pub maxlon: f64,
+        pub maxlat: f64,
     }
+
+
+    #[derive(Debug, Deserialize, Clone, Serialize, Eq, PartialEq, Hash)]
+    pub struct ClientId (pub String);
+
+
+    #[derive(Debug, Deserialize, Clone, Serialize)]
+    pub struct Storage { pub clients: HashMap<ClientId, Client>, }
+
+    impl Storage {
+        pub fn new() -> Self {
+            Storage{ clients: HashMap::new(), }
+        }
+        
+        pub fn add_client(mut self, client: Client) -> Self {
+        self.clients.insert(client.id.clone(), client);
+        self
+    }
+    }
+    // impl warp::Reply for Storage {
+    //     fn into_response(self) -> warp::reply::Response {
+    //         Response::new(format!("{}", self.json).into())
+    //     }
+    // }
+    //
+
 
 
