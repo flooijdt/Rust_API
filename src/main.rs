271016efe925b1f18d4afa312e27085908572631
flooijdt <flooijdt@posteo.de>
2022-09-05 14:33:38 -0300

    Implement logging to display info to stderr

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use warp::{Filter, http::Method, query};
+use tracing_subscriber::fmt::format::FmtSpan;
 mod client;
 mod error;
 mod storage;
@@ -12,31 +13,40 @@ The warp::path() function receives a String with the adress of the desired path,
 `warp` is a framework structured in special functions called `filters`. Each functionality is implemented through these `filters`. */
 #[tokio::main]
 async fn main() {
-    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+    let log_filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "juntos_rust=info,warp=error".to_owned());
 
     log::error!("this is an error!");
     log::info!("this is an info!");
     log::warn!("this is an warning!");
     /* Creates a log filter to log information to stderr */
-    let log = warp::log::custom(|info| {
-        log::info!(
-            "{} {} {} {:#?} from {} with {:#?}",
-            info.method(),
-            info.path(),
-            info.status(),
-            info.elapsed(),
-            info.remote_addr().unwrap(),
-            info.request_headers()
-        );
-    });
+    // let log = warp::log::custom(|info| {
+    //     log::info!(
+    //         "{} {} {} {:#?} from {} with {:#?}",
+    //         info.method(),
+    //         info.path(),
+    //         info.status(),
+    //         info.elapsed(),
+    //         info.remote_addr().unwrap(),
+    //         info.request_headers()
+    //     );
+    // });
+    //
+    tracing_subscriber::fmt()
+        // Use the filter we built above to determine which traces to record.
+        .with_env_filter(log_filter)
+        // Record an event when each span closes. This can be used to time our
+        // routes' durations!
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
+
+
+
 
     /* Creates cors filter. */
     let cors = warp::cors()
         .allow_any_origin()
         .allow_header("content-type")
         .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
-    /* Create log filter to identify each request with a unique id */
-    let id_filter = warp::any().map(|| uuid::Uuid::new_v4().to_string());
 
     /* Gets `Clients` data from juntos server into the `storage` variable. */
     let storage = get_storage().await;
@@ -53,8 +63,15 @@ async fn main() {
         .and(query())
         /* Clones the `storage` so it doesn`t need to be "moved". */
         .and(storage_filter.clone())
-        .and(id_filter)
         .and_then(get_clients);
+        .with(warp::trace(|info| {
+            tracing::info_span!(
+                "get_questions request",
+                method = %info.method(),
+                path = %info.path(),
+                id = %uuid::Uuid::new_v4(),
+            )})
+        );
 
     /* Creates a `filter` for managing `POST` Requests. */
     let add_client = warp::post()
@@ -85,7 +102,7 @@ async fn main() {
         .and_then(delete_client);
 
     /* Creates route to be served by combining all previous `filters` plus the error management module. */
-    let routes = get_clients.or(update_client).or(add_client).or(delete_client).with(log).with(cors).recover(return_error);
+    let routes = get_clients.or(update_client).or(add_client).or(delete_client).with(log).with(cors).with(warp::trace::request()).recover(return_error);
 
     /* Starts server on the below designated port. */
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
