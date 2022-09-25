0c474cd66e06b1be33ecf1a66a1b34765e24f00b
flooijdt <flooijdt@posteo.de>
2022-09-25 15:35:06 -0300

    implementing pagination

--- src/route.rs
+++ src/route.rs
@@ -26,8 +26,7 @@ use crate::storage::{Storage, self};
 /** Implements GET function. */
 // tenho que de algum modo avisar o usuário de que apenas os parametros type e region devem ser oferecidos e sao levados em consideração
 #[instrument]
-pub async fn get_clients(params: HashMap<String, String>, mut storage: Storage, body: HashMap<String, String>) -> Result<warp::reply::Json, Rejection>{
-    println!("{:#?}", &body);
+pub async fn get_clients(params: HashMap<String, String>, mut storage: Storage) -> Result<warp::reply::Json, Rejection>{
     info!("Start querying clients");
     /* Applies pagination parameters provided by query. */
     if !params.is_empty() {
