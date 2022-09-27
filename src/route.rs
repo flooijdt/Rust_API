12ffbf77c3195ce298a3a31704672507d2b1854e
flooijdt <flooijdt@posteo.de>
2022-09-27 14:14:16 -0300

    implementing pagination

--- src/route.rs
+++ src/route.rs
@@ -62,6 +62,12 @@ pub async fn get_clients(params: HashMap<String, String>, mut storage: Storage) 
 
             /* Sets the standard pageSize to 10. */
             warp_response.pageSize = 10; 
+
+            if warp_response.totalCount % 10 == 0 {
+                let total_pgs = warp_response.totalCount / 10;
+
+
+            }
             let total_pgs = warp_response.totalCount / 10;
 
             warp_response.pageSize = params.get("pageSize").expect("Could not get pageSize.").parse::<usize>().expect("Could not parse pageSize to usize.");
