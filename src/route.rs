cdc11bd0f4420526a06d44a960204cccf970827a
flooijdt <flooijdt@posteo.de>
2022-09-28 15:56:26 -0300

    implementing pagination

--- src/route.rs
+++ src/route.rs
@@ -99,9 +99,9 @@ pub async fn get_clients(
                 let total_pgs = (warp_response.totalCount / warp_response.pageSize) + 1;
             }
 
-            let last_n_index = ((warp_response.pageNumber + 1) * warp_response.pageSize);
+            let mut last_n_index = (warp_response.pageNumber + 1) * warp_response.pageSize;
 
-            if last_n_index < warp_response.totalCount {
+            if last_n_index > warp_response.totalCount {
                 last_n_index = warp_response.totalCount;
             }
 
