d68828dfc53706019e76d0ad86eff400b8bf5292
flooijdt <flooijdt@posteo.de>
2022-10-07 15:13:03 -0300

    tries to refuse account with same email.

--- src/account.rs
+++ src/account.rs
@@ -52,9 +52,6 @@ pub async fn add_account(
     match account_given {
         Some(_) => Err(warp::reject::custom(Error::AccountAlreadyExist)),
         None => {
-            if account_given.clone().unwrap().email == account.email {
-                return Err(warp::reject::custom(Error::AccountAlreadyExist));
-            };
             storage.accounts.write().await.insert(
                 account.id.clone().expect("Could not insert id in storage."),
                 account.clone(),
