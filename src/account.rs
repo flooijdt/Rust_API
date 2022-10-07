6acae6ac8cefe0aaca7f3da0a8870aa3716c6ea5
flooijdt <flooijdt@posteo.de>
2022-10-07 15:32:07 -0300

    tries to refuse account with same email.

--- src/account.rs
+++ src/account.rs
@@ -41,14 +41,20 @@ pub async fn add_account(
 ) -> Result<impl warp::Reply, warp::Rejection> {
     /* Hash and salt password before storing it. */
     hash(account.password.as_bytes());
-
+    /* Tries to get the Account via its Id. */
     let account_given = storage
         .accounts
         .write()
         .await
         .get_mut(&account.id.clone().expect("Could not get requested id."))
         .cloned();
-
+    /* Returns Error if it finds same email. */
+    for acc in storage.accounts.read().await.clone().iter() {
+        if acc.1.email == account.email {
+            return Err(warp::reject::custom(Error::AccountAlreadyExist));
+        }
+    }
+    /* returns error if id is already used or adds account if not. */
     match account_given {
         Some(_) => Err(warp::reject::custom(Error::AccountAlreadyExist)),
         None => {
