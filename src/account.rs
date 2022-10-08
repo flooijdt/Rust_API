178f4d3fe478419884d79dd288a42a86af9d6023
flooijdt <flooijdt@posteo.de>
2022-10-08 09:00:21 -0300

    tries to refuse account with same email.

--- src/account.rs
+++ src/account.rs
@@ -43,29 +43,27 @@ pub async fn add_account(
     /* Hash and salt password before storing it. */
     hash(account.password.as_bytes());
     /* Tries to get the Account via its Id. */
-    let account_given = storage
-        .accounts
-        .write()
-        .await
-        .get_mut(&account.id.clone().expect("Could not get requested id."))
-        .cloned();
+    // let account_given = storage
+    //     .accounts
+    //     .write()
+    //     .await
+    //     .get_mut(&account.id.clone().expect("Could not get requested id."))
+    //     .cloned();
+    let mut acc_counter = 1;
     /* Returns Error if it finds same email. */
-    for acc in storage.accounts.read().await.clone().iter() {
+    for mut acc in storage.accounts.read().await.clone().iter_mut() {
         if acc.1.email == account.email {
             return Err(warp::reject::custom(Error::AccountAlreadyExist));
         }
-    }
-    /* returns error if id is already used or adds account if not. */
-    match account_given {
-        Some(_) => Err(warp::reject::custom(Error::AccountIdAlreadyExist)),
-        None => {
-            storage.accounts.write().await.insert(
-                account.id.clone().expect("Could not insert id in storage."),
-                account.clone(),
-            );
-            // .expect("Could not register account id and account.");
-            Ok(warp::reply::with_status("Account added.", StatusCode::OK))
-        }
+        acc_counter += 1;
+        account.id = Some(AccountId(acc_counter).clone()).clone();
+        storage.accounts.write().await.insert(
+            account.id.expect("Could not insert id into storage."),
+            account.clone(),
+        );
+        return Ok(warp::reply::with_status("Account added.", StatusCode::OK));
+
+        // acc.0 = &AccountId(acc_counter).clone();
     }
 }
 
