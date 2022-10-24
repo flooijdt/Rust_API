f8c1bd66be6af5525af93c99dab3c6f1415c1279
flooijdt <flooijdt@posteo.de>
2022-10-24 08:20:38 -0300

    tries to refuse account with same email.

--- src/account.rs
+++ src/account.rs
@@ -49,8 +49,8 @@ pub async fn add_account(
             .accounts
             .write()
             .await
-            .insert(AccountId(1), account.clone());
-        // .expect("Could not insert Account into storage1.");
+            .insert(AccountId(1), account.clone())
+            .expect("Could not insert Account into storage1.");
         Ok(warp::reply::with_status("Account added.1", StatusCode::OK))
     } else {
         for mut acc in storage.accounts.read().await.clone().iter_mut() {
@@ -69,15 +69,14 @@ pub async fn add_account(
                     .accounts
                     .write()
                     .await
-                    .insert(AccountId(acc_id), account.clone());
-                // .expect("Could not insert Account into storage2.");
+                    .insert(AccountId(acc_id), account.clone())
+                    .expect("Could not insert Account into storage2.");
                 println!("Last else statement.3");
                 // .expect("Could not insert Account into storage2.");
                 println!("iterating");
             }
             // return Ok(warp::reply::with_status("Account added.2", StatusCode::OK));
         }
-
         //commentareeo
         Ok(warp::reply::with_status("Account added.2", StatusCode::OK))
         // acc.0 = &AccountId(acc_counter).clone();
